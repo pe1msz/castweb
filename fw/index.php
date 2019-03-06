10bf4d761a28f07bfeed7357169c1b2e095c7778
pe1msz <39623528+pe1msz@users.noreply.github.com>
2019-03-06 21:32:01 +0100

    Update index.php
    modified layout

--- fw/index.php
+++ fw/index.php
@@ -63,14 +63,13 @@ require_once('../config/version.php');
 <form action="upload_radio.php" method="post" enctype="multipart/form-data">
    <table>
   <tr>
-        <h3>DVMEGA-Cast hotsport Radio-board</h3>
+        <h3>DVMEGA-Cast hotspot Radio-board</h3>
   </tr>
   <tr>
         <center>Select Firmware-file to upload to the DVMEGA-Cast hotspot radioboard<BR><BR></center>
         <center><input type="file" name="fileToUpload" id="fileToUpload"><BR><BR></center>
         <center><input type="submit" value="Upload Firmware" name="submit"><BR><BR></center>
   </tr>
-     <th><BR></th>
         </table>
 
 </form>
