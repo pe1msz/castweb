4d77140d0d5df45c321841d69bfdac0177b38982
pe1msz <39623528+pe1msz@users.noreply.github.com>
2019-03-07 20:41:15 +0100

    Update install.sh
    fix in php downloading

--- install.sh
+++ install.sh
@@ -7,7 +7,8 @@ cd /var/www/dashboard/admin/fw
 sudo wget https://www.combitronics.nl/download/index.phph
 sudo mv index.phph index.php
 cd ..
-sudo wget https://www.combitronics.nl/download/cast_update_pages.php
+sudo wget https://www.combitronics.nl/download/cast_update_pages.phph
+sudo mv cast_update_pages.phph cast_update_pages.php
 sudo wget https://www.combitronics.nl/download/cast_update.sh
 sudo chmod +x /var/www/dashboard/admin/cast_update.sh
 
