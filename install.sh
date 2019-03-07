07764cd86facc62dd814c1119986c543b187c6e8
pe1msz <39623528+pe1msz@users.noreply.github.com>
2019-03-07 20:34:52 +0100

    Create install.sh
    created simpler script

---
+++ install.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+sudo mount -o remount,rw /
+
+cd /var/www/dashboard/admin/fw
+
+sudo wget https://www.combitronics.nl/download/index.phph
+sudo mv index.phph index.php
+cd ..
+sudo wget https://www.combitronics.nl/download/cast_update_pages.php
+sudo wget https://www.combitronics.nl/download/cast_update.sh
+sudo chmod +x /var/www/dashboard/admin/cast_update.sh
+
